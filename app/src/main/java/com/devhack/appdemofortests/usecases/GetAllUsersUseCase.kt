709a71c8857a6d36a8ba3e3f9b719f253be4405f
jggomezt <juan@wordbox.ai>
2021-04-13 22:40:04 -0500

    pre-commit test

--- app/src/main/java/com/devhack/appdemofortests/usecases/GetAllUsersUseCase.kt
+++ app/src/main/java/com/devhack/appdemofortests/usecases/GetAllUsersUseCase.kt
@@ -1,12 +1,13 @@
 package com.devhack.appdemofortests.usecases
 
-import co.devhack.base.*
-import co.devhack.base.error.*
+import co.devhack.base.Either
+import co.devhack.base.error.Failure
 import co.devhack.base.usecase.UseCase
 import com.devhack.appdemofortests.usecases.repository.UserRepository
 
 class GetAllUsersUseCase(
     private val userRepository: UserRepository
 ) : UseCase<List<User>, UseCase.None>() {
-    override suspend fun run(params: None): Either<Failure, List<User>> = userRepository.getAllUsers()
+    override suspend fun run(params: None): Either<Failure, List<User>> =
+        userRepository.getAllUsers()
 }
