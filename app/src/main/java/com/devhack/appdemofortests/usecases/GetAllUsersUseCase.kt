5670a3659b1bec48117869ab0b37af1827e1f20f
jggomezt <juan@wordbox.ai>
2021-04-13 22:36:24 -0500

    add pre-commit

--- app/src/main/java/com/devhack/appdemofortests/usecases/GetAllUsersUseCase.kt
+++ app/src/main/java/com/devhack/appdemofortests/usecases/GetAllUsersUseCase.kt
@@ -1,6 +1,6 @@
 package com.devhack.appdemofortests.usecases
 
-import co.devhack.base.Either
+import co.devhack.base.*
 import co.devhack.base.error.*
 import co.devhack.base.usecase.UseCase
 import com.devhack.appdemofortests.usecases.repository.UserRepository
